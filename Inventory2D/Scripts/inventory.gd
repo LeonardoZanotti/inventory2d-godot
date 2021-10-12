84f92ae9d8b9646d7100a4a78922b1a51c30dd1d
LeonardoZanotti <leonardojosezanotti@gmail.com>
2021-10-12 14:16:32 -0300

    resources

---
+++ Inventory2D/Scripts/inventory.gd
@@ -0,0 +1,29 @@
+extends Resource
+
+
+class_name Inventory
+
+signal item_changed(indexes)
+
+export (Array, Resource) var items = [
+	null, null, null, null, null, null, null, null, null
+]
+
+func set_item(item_index, item):
+	var previousItem = items[item_index]
+	items[item_index] = item;
+	emit_signal("item_changed", [item_index])
+	return previousItem
+	
+func swap_items(item_index, target_item_index):
+	var targetItem = items[target_item_index]
+	var item = items[item_index]
+	items[target_item_index] = item
+	items[item_index] = targetItem
+	emit_signal("item_changed", [item_index, target_item_index])	
+	
+func remove_item(item_index):
+	var previousItem = items[item_index]
+	items[item_index] = null;
+	emit_signal("item_changed", [item_index])
+	return previousItem
